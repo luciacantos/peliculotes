5ada5ae850df728f6744e638d11d3cb3b60eeab0
luciacantos <luciacantos22@gmail.com>
2024-12-11 17:27:08 +0100

    corrección registro

--- netflixuax/movies/views.py
+++ netflixuax/movies/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Movie
-
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def home(request):
     movies = Movie.objects.all()
     return render(request, 'movies/home.html', {'movies': movies})
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'movies/register.html', {'form': form})
