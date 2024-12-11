451866d745e264996fb8edfa8c8a3eabf5c1c601
luciacantos <luciacantos22@gmail.com>
2024-12-11 17:22:12 +0100

    muestra de las peliulas

--- netflixuax/movies/views.py
+++ netflixuax/movies/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Movie
+
 
 # Create your views here.
+def home(request):
+    movies = Movie.objects.all()
+    return render(request, 'movies/home.html', {'movies': movies})
