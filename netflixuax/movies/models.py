e616398e6b26e2c228d8de426bef4c624b00e533
luciacantos <luciacantos22@gmail.com>
2024-12-11 18:14:23 +0100

    corrección página principal

--- netflixuax/movies/models.py
+++ netflixuax/movies/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Movie(models.Model):
@@ -10,3 +11,14 @@ class Movie(models.Model):
 
     def __str__(self):
         return self.title
+
+# paara crear lista de peliculitas favoritas
+class FavouriteMovie(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('user', 'movie')
+
+    def __str__(self):
+        return f"{self.user.username} - {self.movie.title}"
--- netflixuax/movies/views.py
+++ netflixuax/movies/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render, redirect
-from .models import Movie
+from .models import Movie, FavouriteMovie
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
 
 # Create your views here.def home(request):
 def home(request):
+    if not request.user.is_authenticated:
+
+        if request.method == 'POST':
+            form = AuthenticationForm(data=request.POST)
+            if form.is_valid():
+                user = form.get_user()
+                login(request, user)
+                return redirect('home')
+        else:
+            form = AuthenticationForm()
+        return render(request, 'movies/login.html', {'form': form})
+
     movies = Movie.objects.all()
     return render(request, 'movies/home.html', {'movies': movies})
 
@@ -17,3 +32,22 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'movies/register.html', {'form': form})
+
+
+@login_required
+def add_movie(request, movie_id):
+    movie = Movie.objects.get(id=movie_id)
+    FavoriteMovie.objects.get_or_create(user=request.user, movie=movie)
+    return redirect('home')
+
+@login_required
+def remove_favorite(request, movie_id):
+    movie = Movie.objects.get(id=movie_id)
+    FavoriteMovie.objects.filter(user=request.user, movie=movie).delete()
+    return redirect('home')
+
+@login_required
+def remove_favorite(request, movie_id):
+    movie = Movie.objects.get(id=movie_id)
+    FavoriteMovie.objects.filter(user=request.user, movie=movie).delete()
+    return redirect('home')
