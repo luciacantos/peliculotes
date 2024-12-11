781b69e532c00cacaf33fb99077172f981d8dac4
luciacantos <luciacantos22@gmail.com>
2024-12-11 17:23:02 +0100

    login

--- netflixuax/netflixuax/urls.py
+++ netflixuax/netflixuax/urls.py
@@ -22,3 +22,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('movies.urls')),
+    path('login/', auth_views.LoginView.as_view(template_name='movies/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+]
