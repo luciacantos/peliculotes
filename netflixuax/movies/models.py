6939ee23d97597e7bde9769141bd1ab2646e815b
luciacantos <luciacantos22@gmail.com>
2024-12-11 17:10:06 +0100

    .env, modelar pleiculas

--- netflixuax/movies/models.py
+++ netflixuax/movies/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    tmdb_id = models.IntegerField(unique=True)
+    title = models.CharField(max_length=255)
+    overview = models.TextField(null=True, blank=True)
+    release_date = models.DateField(null=True, blank=True)
+    poster_path = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
