12a2b00d8fcf509447e88b092f66e6017b09a851
luciacantos <luciacantos22@gmail.com>
2024-12-18 00:15:27 +0100

    railway

--- netflixuax/netflixuax/settings.py
+++ netflixuax/netflixuax/settings.py
@@ -82,11 +82,14 @@ WSGI_APPLICATION = "netflixuax.wsgi.app"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": config("DB_HOST"),
+        "PORT": config("DB_PORT", cast=int),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
